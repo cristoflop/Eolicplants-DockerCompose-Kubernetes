6788cf92def25af2399341971a8af1993107d7e1
Juan Avila <dev@juanavila.es>
2021-03-21 13:33:47 +0100

    Add retry

--- toposervice/src/main/java/es/codeurjc/mastercloudapps/topo/Application.java
+++ toposervice/src/main/java/es/codeurjc/mastercloudapps/topo/Application.java
@@ -2,11 +2,19 @@ package es.codeurjc.mastercloudapps.topo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.policy.AlwaysRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        RetryTemplate template = new RetryTemplate();
+        AlwaysRetryPolicy policy = new AlwaysRetryPolicy();
+        template.setRetryPolicy(policy);
+        template.execute(context -> {
+            SpringApplication.run(Application.class, args);
+            return true;
+        });
     }
 }
