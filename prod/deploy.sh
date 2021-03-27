670a778d994040c746c7f33f59ce54e4d424cf14
Juan Avila <dev@juanavila.es>
2021-03-27 21:52:54 +0100

    Update script

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
   echo "Usage:"
-  echo "arg[0] => output directory/file of generated manifests (otherwise files will be deleted)"
+  echo "arg[0] => output directory of generated manifests (otherwise files will be deleted)"
   exit 0
 fi
 
@@ -13,13 +13,16 @@ else
   mkdir "$1"
   kompose convert -o "$1"
 fi
+
 kubectl delete --all services
 kubectl delete --all deployments
+
 if [ "$1" = "" ]; then
   kubectl apply -f .manifests.yaml
 else
   kubectl apply -f "$1"
 fi
+
 if [ "$1" = "" ]; then
   rm .manifests.yaml
 fi
