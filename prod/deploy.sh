c744fe6180b10731f7c3787fd2607fcc625d9221
Juan Avila <dev@juanavila.es>
2021-03-27 21:30:19 +0100

    Improve deploy.sh

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage:"
+  echo "arg[0] => output directory/file of generated manifests (otherwise files will be deleted)"
+  exit 0
+fi
+
 export KUBECONFIG=$HOME/Downloads/okteto-kube.config:${KUBECONFIG:-$HOME/.kube/config}
-kompose convert
+
+if [ "$1" = "" ]; then
+  kompose convert -o .manifests.yaml
+else
+  mkdir "$1"
+  kompose convert -o "$1"
+fi
 kubectl delete --all services
 kubectl delete --all deployments
-for file in *.yaml
-do
-   kubectl apply -f "$file"
-done
+if [ "$1" = "" ]; then
+  kubectl apply -f .manifests.yaml
+else
+  kubectl apply -f "$1"
+fi
+if [ "$1" = "" ]; then
+  rm .manifests.yaml
+fi
