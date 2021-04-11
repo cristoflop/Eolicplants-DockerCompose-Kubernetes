a6535c96b889df2bd939e6f7c0261957e6bf884c
cristoflop <cristofer.lc.99@gmail.com>
2021-04-11 03:40:59 +0200

    Added deploy.cmd for deploying in order deployments and services

---
+++ prod/deploy.cmd
@@ -0,0 +1,22 @@
+:: PRIMERO ES NECESARIO TENER CONFIGURADO EL OKTETO-CONFIG PARA QUE SE PUBLIQUEN LOS SERVICIOS EN LA WEB DE OKTETO CLOUD
+
+kubectl delete --all services
+kubectl delete --all deployments
+
+kubectl apply -f broker-deployment.yaml
+kubectl apply -f sqldb-deployment.yaml
+kubectl apply -f nosqldb-deployment.yaml
+kubectl apply -f topo-deployment.yaml
+kubectl apply -f weather-deployment.yaml
+kubectl apply -f planner-deployment.yaml
+kubectl apply -f server-deployment.yaml
+
+kubectl apply -f broker-service.yaml
+kubectl apply -f sqldb-service.yaml
+kubectl apply -f nosqldb-service.yaml
+kubectl apply -f topo-service.yaml
+kubectl apply -f weather-service.yaml
+kubectl apply -f planner-service.yaml
+kubectl apply -f server-service.yaml
+
+kubectl apply -f ingress.yaml
