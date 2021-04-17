de4453d173b6dc91828c5c2aededaccd0b17ba21
Juan Avila <dev@juanavila.es>
2021-04-17 14:33:13 +0200

    Using helm

--- build.cmd
+++ build.cmd
@@ -1,14 +1,14 @@
 
-docker build ./server -t cristoflop/server:latest
-docker push cristoflop/server:latest
+docker build ./server -t cloudappsteam/server:1.0
+docker push cloudappsteam/server:1.0
 
-pack build cristoflop/weatherservice:latest --path ./weatherservice --builder gcr.io/buildpacks/builder:v1
-docker push cristoflop/weatherservice:latest
+pack build cloudappsteam/weatherservice:1.0 --path ./weatherservice --builder gcr.io/buildpacks/builder:v1
+docker push cloudappsteam/weatherservice:1.0
 
-docker build ./planner -t cristoflop/planner:latest
-docker push cristoflop/planner:latest
+docker build ./planner -t cloudappsteam/planner:1.0
+docker push cloudappsteam/planner:1.0
 
 cd toposervice
 mvn clean
-mvn compile jib:build -Dimage=cristoflop/toposervice:latest
+mvn compile jib:build -Dimage=cloudappsteam/toposervice:1.0
 cd ..
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKERHUB_NAME=cristoflop
+DOCKERHUB_NAME=cloudappsteam
 
 option=$1
 
@@ -16,39 +16,39 @@ if [ $build = true ]; then
   echo "-------------------"
   echo "Building server..."
   echo "-------------------"
-  docker build ./server -t "$DOCKERHUB_NAME/server":latest
+  docker build ./server -t "$DOCKERHUB_NAME/server":1.0
 fi
 if [ $push = true ]; then
   echo "-------------------"
   echo "Pushing server..."
   echo "-------------------"
-  docker push "$DOCKERHUB_NAME/server":latest
+  docker push "$DOCKERHUB_NAME/server":1.0
 fi
 
 if [ $build = true ]; then
   echo "-------------------"
   echo "Building weatherservice..."
   echo "-------------------"
-  pack build "$DOCKERHUB_NAME/weatherservice":latest --path ./weatherservice --builder gcr.io/buildpacks/builder:v1
+  pack build "$DOCKERHUB_NAME/weatherservice":1.0 --path ./weatherservice --builder gcr.io/buildpacks/builder:v1
 fi
 if [ $push = true ]; then
   echo "-------------------"
   echo "Pushing weatherservice..."
   echo "-------------------"
-  docker push "$DOCKERHUB_NAME/weatherservice":latest
+  docker push "$DOCKERHUB_NAME/weatherservice":1.0
 fi
 
 if [ $build = true ]; then
   echo "-------------------"
   echo "Building planner..."
   echo "-------------------"
-  docker build ./planner -t "$DOCKERHUB_NAME/planner":latest
+  docker build ./planner -t "$DOCKERHUB_NAME/planner":1.0
 fi
 if [ $push = true ]; then
   echo "-------------------"
   echo "Pushing planner..."
   echo "-------------------"
-  docker push "$DOCKERHUB_NAME/planner":latest
+  docker push "$DOCKERHUB_NAME/planner":1.0
 fi
 
 if [ $build = true ] && [ $push = true ]; then
@@ -56,7 +56,7 @@ if [ $build = true ] && [ $push = true ]; then
   echo "Building and pushing toposervice..."
   echo "-------------------"
   cd toposervice || exit
-  mvn compile jib:build -Dimage="$DOCKERHUB_NAME/toposervice":latest
+  mvn compile jib:build -Dimage="$DOCKERHUB_NAME/toposervice":1.0
   cd ..
 fi
 if [ $build = true ] && [ $push = false ]; then
@@ -64,6 +64,12 @@ if [ $build = true ] && [ $push = false ]; then
   echo "Building toposervice..."
   echo "-------------------"
   cd toposervice || exit
-  mvn compile jib:dockerBuild -Dimage="$DOCKERHUB_NAME/toposervice":latest
+  mvn compile jib:dockerBuild -Dimage="$DOCKERHUB_NAME/toposervice":1.0
   cd ..
 fi
+
+
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=eolicplants,app.kubernetes.io/instance=eolicplants-release" -o jsonpath="{.items[0].metadata.name}")
+  export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+  echo "Visit http://127.0.0.1:8080 to use your application"
+  kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
